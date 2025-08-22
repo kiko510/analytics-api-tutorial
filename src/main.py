c1c0e3966a8d7821337c455eeafb4ddcea2b1989
Kristián Peter <peter.kristian149@gmail.com>
2025-08-22 18:06:58 +0200

    add events router

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from typing import Union
 
 from fastapi import FastAPI
 
-app = FastAPI()
+from api.events.routing import router as event_router
 
+
+app = FastAPI()
+app.include_router(event_router, prefix='/api/events')
 
 @app.get("/")
 def read_root():
