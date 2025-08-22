c89d79c10564ac2f13de7efbba0b255223cd42a4
Kristián Peter <peter.kristian149@gmail.com>
2025-08-22 17:25:22 +0200

    add health endpoint

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] =  None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/healthz")
+def read_api_health():
+    return {"status": "ok"}
