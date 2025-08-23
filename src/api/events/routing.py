0e9106358c71427d4e8d0449b5426f72fb4fb9df
Kristián Peter <peter.kristian149@gmail.com>
2025-08-23 14:30:47 +0200

    add basic event schema

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
 
+from .schemas import EventSchema
+
+
 router = APIRouter()
+
 
 @router.get("/")
 def read_events():
     return {
-		"items": [1, 2, 3]
+		"results": [1, 2, 3]
 	}
+
+
+@router.get("/{event_id}") 
+def get_event(event_id:int) -> EventSchema:
+    return {"id": event_id}
