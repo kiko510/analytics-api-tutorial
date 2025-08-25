5ef570afd958062f820e7785c03e870785de37e2
Kristián Peter <peter.kristian149@gmail.com>
2025-08-25 16:23:27 +0200

    add other endpoints

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,16 +1,35 @@
 from fastapi import APIRouter
 
-from .schemas import EventSchema
+from .schemas import (
+    EventSchema, 
+    EventListSchema,
+    EventCreateSchema,
+    EventUpdateSchema,
+)
 
 
 router = APIRouter()
 
 
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     return {
-		"results": [1, 2, 3]
+		"results": [
+			{"id": 1}, {"id": 2}, {"id": 3}
+        ],
+		"count": 3,
 	}
+
+@router.post("/")
+def create_event(payload:EventCreateSchema) -> EventSchema:
+    data = payload.model_dump()
+    return {"id": 123, **data}
+
+
+@router.put("/{event_id}")
+def update_event(event_id:int, payload:EventUpdateSchema) -> EventSchema:
+    data = payload.model_dump()
+    return {"id": event_id, **data}
 
 
 @router.get("/{event_id}") 
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,5 +1,20 @@
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, Field
 
 
 class EventSchema(BaseModel):
     id: int
+    page: str | None = ""
+    description: str | None = ""
+    
+class EventCreateSchema(BaseModel):
+    page: str
+    description: str | None = Field(default="default description")
+    
+class EventUpdateSchema(BaseModel):
+    description: str
+    
+    
+class EventListSchema(BaseModel):
+    results: list[EventSchema]
+    count: int
